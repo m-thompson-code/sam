c277e4582967ea2ab7e62ab707dd42095bebb7ed
Mark Thompson <moomoomamoo@gmail.com>
2018-04-19 19:58:21 -0700

    animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,10 @@ import * as firebase from "firebase";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	aLength: number;
+	mode: string;
+	modeTimeout: any;
+
+	showOthers: boolean;
 
 	topUrls: any;
 	bottomUrls: any;
@@ -20,23 +23,23 @@ export class AppComponent {
   	}
 
 	ngOnInit() {
+		this.mode = 'light';
+
 		this.topUrls = [
-			{text: 'PRIME', url: ''},
-			{text: 'SPARROW', url: ''},
-			{text: 'SISTERCITY', url: ''},
-			{text: 'USEMBASSY', url: ''},
-			{text: "VETERANS'MEMORIAL", url: ''},
-			{text: 'ARVOPARTCENTER', url: ''},
+			{text: 'PRIME', href: 'https://www.instagram.com/explore/tags/pr02_prime/'},
+			{text: 'SPARROW', href: 'https://www.dwell.com/article/a-designer-completely-transforms-a-little-1950s-house-with-dollar125k-a5e9d1d6'},
+			{text: 'SISTERCITY', href: 'https://www.architecturaldigest.com/story/ace-hotel-sister-city-bowery-manhattan-lower-east-side'},
+			{text: 'USEMBASSY', href: 'https://alliedworks.com/projects/us-embassy-mozambique'},
+			{text: "VETERANS'MEMORIAL", href: 'https://alliedworks.com/projects/ohio-veterans-memorial-and-museum'},
+			{text: 'ARVOPARTCENTER', href: 'https://alliedworks.com/projects/arvo-part-centre'},
 		];
 
 		this.bottomUrls = [
-			{text: 'UCSANTACRUZ', url: ''},
-			{text: 'CLEMSONUNIVERSITY', url: ''},
-			{text: 'PRESERVE24', url: ''},
-			{text: 'THESTEINWAY', url: ''},
+			{text: 'UCSANTACRUZ', href: 'https://alliedworks.com/projects/institute-of-arts-and-sciences-ucsc'},
+			{text: 'CLEMSONUNIVERSITY', href: 'https://alliedworks.com/projects/spaulding-paolozzi-center'},
+			{text: 'PRESERVE24', href: 'https://www.urbandaddy.com/articles/23858/new-york/preserve-24-preserve-the-right-a-massive-den-made-of-pianos-and-boats'},
+			{text: 'THESTEINWAY', href: 'https://hotpads.com/1-bed-800-sqft-2395-los-angeles-ca-90026-1m2mh4q/pad'},
 		];
-		// this.aLength = 70;
-		// firebase.database().ref().update({moo: 'cow'});
 	}
 
 	getUrlWordSpacing() {
@@ -46,6 +49,27 @@ export class AppComponent {
 
 		return (this.linksContainer.nativeElement.offsetWidth - this.topUrlsSpan.nativeElement.offsetWidth) / (this.topUrls.length - 1);
 	}
+
+	toggleMode() {
+		// clearTimeout(this.modeTimeout);
+
+		if (this.mode === 'light') {
+			this.mode = '';
+			this.modeTimeout = setTimeout(() => {
+				this.mode = 'dark';
+				this.modeTimeout = setTimeout(() => {
+					this.showOthers = true;
+				}, 1000);
+			}, 500);
+		} else {
+			this.mode = '';
+			this.modeTimeout = setTimeout(() => {
+				this.mode = 'light';
+				this.showOthers = false;
+			}, 1000);
+		}
+	}
+
 	// https://scotch.io/tutorials/responsive-equal-height-with-angular-directive
 	@HostListener('window:resize')
     onResize() {
