01a57a47c960e1659cc116aecf9f52a62611a8eb
Mark Thompson <moomoomamoo@gmail.com>
2018-04-28 20:51:57 -0700

    ugh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,41 +27,41 @@ export class AppComponent {
 	ngOnInit() {
 		this.mode = 'light';
 
-		this.topUrls = [
-			{text: 'PRIME', href: 'https://www.instagram.com/explore/tags/pr02_prime/'},
-			{text: 'SPARROW', href: 'https://www.dwell.com/article/a-designer-completely-transforms-a-little-1950s-house-with-dollar125k-a5e9d1d6'},
-			{text: 'SISTERCITY', href: 'https://www.architecturaldigest.com/story/ace-hotel-sister-city-bowery-manhattan-lower-east-side'},
-			{text: 'USEMBASSY', href: 'https://alliedworks.com/projects/us-embassy-mozambique'},
-			{text: "VETERANS'MEMORIAL", href: 'https://alliedworks.com/projects/ohio-veterans-memorial-and-museum'},
-			{text: 'ARVOPÄRTCENTER', href: 'https://alliedworks.com/projects/arvo-part-centre'},
+		this.urls = [
+			{y: 0, text: 'PRIME', href: 'https://www.instagram.com/explore/tags/pr02_prime/'},
+			{y: 0, text: 'SPARROW', href: 'https://www.dwell.com/article/a-designer-completely-transforms-a-little-1950s-house-with-dollar125k-a5e9d1d6'},
+			{y: 0, text: 'SISTERCITY', href: 'https://www.architecturaldigest.com/story/ace-hotel-sister-city-bowery-manhattan-lower-east-side'},
+			{y: 0, text: 'USEMBASSY', href: 'https://alliedworks.com/projects/us-embassy-mozambique'},
+			{y: 0, text: "VETERANS'MEMORIAL", href: 'https://alliedworks.com/projects/ohio-veterans-memorial-and-museum'},
+			{y: 0, text: 'ARVOPÄRTCENTER', href: 'https://alliedworks.com/projects/arvo-part-centre'},
+		// ];
+
+		// this.bottomUrls = [
+			{y: 0, text: 'UCSANTACRUZ', href: 'https://alliedworks.com/projects/institute-of-arts-and-sciences-ucsc'},
+			{y: 0, text: 'CLEMSONUNIVERSITY', href: 'https://alliedworks.com/projects/spaulding-paolozzi-center'},
+			{y: 0, text: 'PRESERVE24', href: 'https://www.urbandaddy.com/articles/23858/new-york/preserve-24-preserve-the-right-a-massive-den-made-of-pianos-and-boats'},
+			{y: 0, text: 'THESTEINWAY', href: 'https://hotpads.com/1-bed-800-sqft-2395-los-angeles-ca-90026-1m2mh4q/pad'},
 		];
 
-		this.bottomUrls = [
-			{text: 'UCSANTACRUZ', href: 'https://alliedworks.com/projects/institute-of-arts-and-sciences-ucsc'},
-			{text: 'CLEMSONUNIVERSITY', href: 'https://alliedworks.com/projects/spaulding-paolozzi-center'},
-			{text: 'PRESERVE24', href: 'https://www.urbandaddy.com/articles/23858/new-york/preserve-24-preserve-the-right-a-massive-den-made-of-pianos-and-boats'},
-			{text: 'THESTEINWAY', href: 'https://hotpads.com/1-bed-800-sqft-2395-los-angeles-ca-90026-1m2mh4q/pad'},
-		];
+		// this.urls = [];
 
-		this.urls = [];
-
-		for (var i = 0; i < this.topUrls.length; i++) {
-			this.urls.push(this.topUrls[i]);
-		}
-		for (var i = 0; i < this.bottomUrls.length; i++) {
-			this.urls.push(this.bottomUrls[i]);
-		}
+		// for (var i = 0; i < this.topUrls.length; i++) {
+		// 	this.urls.push(this.topUrls[i]);
+		// }
+		// for (var i = 0; i < this.bottomUrls.length; i++) {
+		// 	this.urls.push(this.bottomUrls[i]);
+		// }
 
 		console.log(this.urls);
 	}
 
-	getUrlWordSpacing() {
-		if (!this.linksContainer || !this.topUrlsSpan || this.topUrls) {
-			return -1;
-		}
+	// getUrlWordSpacing() {
+	// 	if (!this.linksContainer || !this.topUrlsSpan || this.topUrls) {
+	// 		return -1;
+	// 	}
 
-		return (this.linksContainer.nativeElement.offsetWidth - this.topUrlsSpan.nativeElement.offsetWidth) / (this.topUrls.length - 1);
-	}
+	// 	return (this.linksContainer.nativeElement.offsetWidth - this.topUrlsSpan.nativeElement.offsetWidth) / (this.topUrls.length - 1);
+	// }
 
 	toggleMode() {
 		// clearTimeout(this.modeTimeout);
@@ -83,11 +83,25 @@ export class AppComponent {
 		}
 	}
 
+	getUrlColCount(y: number) {
+		var count = 0;
+
+		if (y === 0) {
+			return 1;
+		}
+
+		for (var i = 0; i < this.urls.length; i++) {
+			if (this.urls[i].y === y) {count += 1};
+		}
+
+		return count || 1;
+	}
+
 	// https://scotch.io/tutorials/responsive-equal-height-with-angular-directive
 	@HostListener('window:resize')
     onResize() {
         // // call our matchHeight function here
         // this.matchHeight(this.el.nativeElement, this.myMatchHeight);
-        console.log(this.getUrlWordSpacing());
+        // console.log(this.getUrlWordSpacing());
     }
 }
--- src/app/textPlacementTest/textPlacementTest.component.ts
+++ src/app/textPlacementTest/textPlacementTest.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, ViewChildren, QueryList, ElementRef } from '@angular/core';
+import { Component, HostListener, Input, ViewChild, ViewChildren, QueryList, ElementRef } from '@angular/core';
 
 import { SpacedComponent } from '../spaced/spaced.component';
 
@@ -9,17 +9,21 @@ import { SpacedComponent } from '../spaced/spaced.component';
   providers: [ ]
 })
 export class TextPlacementTestComponent {
- 	@Input() strs[];
+ 	@Input() urls: any[];
 
 	@ViewChild("container") container: ElementRef;
 
 	// source: https://stackoverflow.com/questions/40165294/access-multiple-viewchildren-using-viewchild
 	@ViewChildren('strComponent', {read: ElementRef}) strViewChildren: QueryList<SpacedComponent>;
-	strCompents: any[];
+	strComponents: any[];
+	// s: any[];
 
 	viewLoaded: boolean;
 
 	JSON: any;
+
+	@Input() containerWidth: number;
+	@Input() marginRight: number;
 
 	constructor() {
 	}
@@ -30,25 +34,57 @@ export class TextPlacementTestComponent {
 	}
 
 	ngAfterViewInit() {
-		// this.viewLoaded = true;
-		// this.strComponents = [];
+		console.log(this.container);
+		console.log(this.container.nativeElement.getBoundingClientRect().y);
 
-
+		console.log(this.strViewChildren);
+		
 		setTimeout(() => {
-			// this.viewLoaded = true;
-			// console.log(this.strViewChildren);
-		// }, 1);
-		setTimeout(() => {
-			this.viewLoaded = true;
-			
-			console.log(this.container);
-			console.log(this.container.nativeElement.getBoundingClientRect().y);
 			this.strComponents = [];
+			// this.s = [];
+
 			this.strViewChildren.forEach(str => {
 				this.strComponents.push(str);
+				// this.s.push(0);
+
 				// console.log(str);
-				console.log(str.nativeElement.getBoundingClientRect().y);
+				// console.log(str.nativeElement.getBoundingClientRect().y);
 			});
+
+			this.getStringRows();
+			setTimeout(() => {
+				this.viewLoaded = true;
+			}, 1);
 		}, 1);
 	}
+
+	getStringRows() {
+		this.viewLoaded = false;
+		setTimeout(() => {
+			if (this.strViewChildren && this.strComponents) {
+				var y = 0, rowHeight = 0;
+				for (var i = 0; i < this.strComponents.length; i++) {
+					if (this.strComponents[i].nativeElement.getBoundingClientRect().y > rowHeight) {
+						y += 1;
+						rowHeight = this.strComponents[i].nativeElement.getBoundingClientRect().y;
+					}
+
+					this.urls[i].y = y;
+					console.log(this.urls[i].y);
+				}	
+			}
+
+			setTimeout(() => {
+				this.viewLoaded = true;
+			}, 1);
+		}, 1);
+	}
+
+	// https://scotch.io/tutorials/responsive-equal-height-with-angular-directive
+	@HostListener('window:resize')
+    onResize() {
+        // // call our matchHeight function here
+        // this.matchHeight(this.el.nativeElement, this.myMatchHeight);
+        console.log(this.getStringRows());
+    }
 }
