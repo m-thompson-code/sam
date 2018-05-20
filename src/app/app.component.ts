80abe9b2036a22eedcb289b31ea90377874dd3a1
Mark Thompson <moomoomamoo@gmail.com>
2018-05-19 21:16:21 -0700

    compressed images and 85% size on icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,7 +46,11 @@ export class AppComponent {
 		this.w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 		this.h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-		this.mode = 'light';
+		this.mode = '';
+		this.modeTimeout = setTimeout(() => {
+			this.mode = 'light';
+			this.showOthers = false;
+		}, 1000);
 
 		this.maxUrls = 6;
 
@@ -55,19 +59,19 @@ export class AppComponent {
 		this.urls = [
 			{width: 0, text: 'PRIME', href: 'https://www.instagram.com/explore/tags/pr02_prime/'},
 			{width: 0, text: 'SPARROW', href: 'https://www.dwell.com/article/a-designer-completely-transforms-a-little-1950s-house-with-dollar125k-a5e9d1d6'},
-			{width: 0, text: 'SISTERCITY', href: 'https://www.architecturaldigest.com/story/ace-hotel-sister-city-bowery-manhattan-lower-east-side'},
+			{width: 0, text: 'SISTER CITY', href: 'https://www.architecturaldigest.com/story/ace-hotel-sister-city-bowery-manhattan-lower-east-side'},
 			{width: 0, text: 'EX NIHILO', href: 'https://www.instagram.com/explore/tags/pr00_exnihilo/'},
-			{width: 0, text: 'USEMBASSY', href: 'https://alliedworks.com/projects/us-embassy-mozambique'},
-			{width: 0, text: "VETERANS\\MEMORIAL", href: 'https://alliedworks.com/projects/ohio-veterans-memorial-and-museum'},
-			{width: 0, text: 'ARVOPÄRT\\CENTER', href: 'https://alliedworks.com/projects/arvo-part-centre'},
-			{width: 0, text: 'UCSANTACRUZ', href: 'https://alliedworks.com/projects/institute-of-arts-and-sciences-ucsc'},
-			{width: 0, text: 'CLEMSON\\UNIVERSITY', href: 'https://alliedworks.com/projects/spaulding-paolozzi-center'},
-			{width: 0, text: 'PRESERVE24', href: 'https://www.urbandaddy.com/articles/23858/new-york/preserve-24-preserve-the-right-a-massive-den-made-of-pianos-and-boats'},
+			{width: 0, text: 'US EMBASSY', href: 'https://alliedworks.com/projects/us-embassy-mozambique'},
+			{width: 0, text: "VETERANS\\sMEMORIAL", href: 'https://alliedworks.com/projects/ohio-veterans-memorial-and-museum'},
+			{width: 0, text: 'ARVOPÄRT\\sCENTER', href: 'https://alliedworks.com/projects/arvo-part-centre'},
+			{width: 0, text: 'UC SANTA CRUZ', href: 'https://alliedworks.com/projects/institute-of-arts-and-sciences-ucsc'},
+			{width: 0, text: 'CLEMSON\\sUNIVERSITY', href: 'https://alliedworks.com/projects/spaulding-paolozzi-center'},
+			{width: 0, text: 'PRESERVE 24', href: 'https://www.urbandaddy.com/articles/23858/new-york/preserve-24-preserve-the-right-a-massive-den-made-of-pianos-and-boats'},
 			// {width: 0, text: 'THESTEINWAY', href: 'https://hotpads.com/1-bed-800-sqft-2395-los-angeles-ca-90026-1m2mh4q/pad'},
 		];
 
 		this.footerUrls = [
-			{width: 0, text: 'contact@\\samanthamink.com', href: 'mailto:contact@samanthamink.com'},
+			{width: 0, text: 'contact@\\nsamanthamink.com', href: 'mailto:contact@samanthamink.com'},
 			{width: 0, text: '@_samanthamink_', href: 'https://www.instagram.com/_samanthamink_/'},
 			{width: 0, text: '+13109682148', href: 'tel:+13109682148'},
 			{width: 0, text: 'culvercity,ca', href: 'https://goo.gl/maps/EDdHuFLuv882'},
@@ -90,7 +94,9 @@ export class AppComponent {
 			this.modeTimeout = setTimeout(() => {
 				this.mode = 'dark';
 				this.modeTimeout = setTimeout(() => {
-					this.showOthers = true;
+					if (this.mode === 'dark') {
+						this.showOthers = true;
+					}
 				}, 2000);
 			}, 500);
 		} else {
